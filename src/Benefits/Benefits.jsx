6ae10262d9b393d75e0cd50154dd05496aaf7010
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-20 12:59:36 +0400

    add benefits.module.scss

--- src/Benefits/Benefits.jsx
+++ src/Benefits/Benefits.jsx
@@ -1,89 +1,90 @@
 import React from "react";
+import cl from "./Benefits.module.scss"
 
 const Benefits = () => {
   return (
     <>
-      <section className="benefits" id="Benefits">
-        <div className="benefits__container">
-          <div className="benefits__images">
+      <section className={cl.benefits} id="Benefits">
+        <div className={cl.benefits__container}>
+          <div className={cl.benefits__images}>
             <img
               src="/images/Ellipse 27.png"
-              className="benefits__image27"
+              className={cl.benefits__image27}
               alt="Ellipse 27"
             />
             <img
               src="/images/Ellipse 223.png"
-              className="benefits__image223"
+              className={cl.benefits__image223}
               alt="Ellipse 223"
             />
             <img
               src="/images/Ellipse 126.png"
-              className="benefits__image126"
+              className={cl.benefits__image126}
               alt="Ellipse 126"
             />
             <img
               src="/images/Ellipse 266.png"
-              className="benefits__image266"
+              className={cl.benefits__image266}
               alt="Ellipse 266"
             />
             <img
               src="/images/Ellipse 125.png"
-              className="benefits__image125"
+              className={cl.benefits__image125}
               alt="Ellipse 125"
             />
           </div>
-          <div className="header__wrapper">
-            <h1 className="benefits__title">Что даст тебе обучение?</h1>
+          <div className={cl.header__wrapper}>
+            <h1 className={cl.benefits__title}>Что даст тебе обучение?</h1>
           </div>
-          <div className="benefits__cards">
-            <div className="cards__wrapper">
-              <div className="benefits__shadows"></div>
-              <div className="benefits__card">
+          <div className={cl.benefits__cards}>
+            <div className={cl.cards__wrapper}>
+              <div className={cl.benefits__shadows}></div>
+              <div className={cl.benefits__card}>
                 <div className="card__imgWrapper">
                   <img
-                    className="card__img"
+                    className={cl.card__img}
                     src="/images/Group 138.png"
                     alt="image"
                   />
                 </div>
-                <p className="benefits__text1">
+                <p className={cl.benefits__text1}>
                   Откроешь свой первый <br /> криптокошелек и научишься <br /> с
                   ним работать
                 </p>
               </div>
             </div>
-            <div className="cards__wrapper">
-              <div className="benefits__shadows"></div>
-              <div className="benefits__card">
+            <div className={cl.cards__wrapper}>
+              <div className={cl.benefits__shadows}></div>
+              <div className={cl.benefits__card}>
                 <img
-                  className="card__img"
+                  className={cl.card__img}
                   src="/images/Group 3290.png"
                   alt=""
                 />
-                <p className="benefits__text2">
+                <p className={cl.benefits__text2}>
                   Поймёшь, как выбирать
                   <br /> правильные дропы
                 </p>
               </div>
             </div>
-            <div className="cards__wrapper">
-              <div className="benefits__shadows"></div>
-              <div className="benefits__card">
-                <img className="card__img" src="/images/Group 140.png" alt="" />
-                <p className="benefits__text3">
+            <div className={cl.cards__wrapper}>
+              <div className={cl.benefits__shadows}></div>
+              <div className={cl.benefits__card}>
+                <img className={cl.card__img} src="/images/Group 140.png" alt="" />
+                <p className={cl.benefits__text3}>
                   Построишь план <br /> по быстрому приумножению заработанных
                   средств
                 </p>
               </div>
             </div>
-            <div className="cards__wrapper">
-              <div className="benefits__card">
-                <img className="card__img" src="/images/Group 142.png" alt="" />
-                <p className="benefits__text4">
+            <div className={cl.cards__wrapper}>
+              <div className={cl.benefits__card}>
+                <img className={cl.card__img} src="/images/Group 142.png" alt="" />
+                <p className={cl.benefits__text4}>
                   Узнаешь кто такие холдеры и флипперы
                 </p>
               </div>
-              <div className="benefits__shadows"></div>
+              <div className={cl.benefits__shadows}></div>
             </div>
           </div>
         </div>
