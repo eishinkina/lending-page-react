9b29ddd9986d120f6fc82688f1efb94944b48c57
Ekaterina Ishinkina <esiforlearn@gmail.com>
2023-08-20 12:46:25 +0400

    add banner.module.scss

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,64 +1,68 @@
 import React from "react";
 import { useState } from "react";
 import MyModal from "../Modal/MyModal";
+import cl from "./Banner.module.scss";
 
 const Banner = () => {
   const [modalVisible, setModalVisible] = useState(false);
   return (
     <>
       <MyModal visible={modalVisible} setVisible={setModalVisible} />
-      <main className="main__container">
-        <section className="main">
-          <article className="main__content">
+      <main className={cl.main__container}>
+        <section className={cl.main}>
+          <article className={cl.main__content}>
             <img
-              className="image__el"
+              className={cl.image__el}
               src="/images/Ellipse 25.png"
               alt="Абстрактное изображение"
             />
-            <h1 className="main__title">
+            <h1 className={cl.main__title}>
               Не упусти
               <br /> возможность войти в <b>прибыльную нишу</b>
             </h1>
-            <p className="main__text">
+            <p className={cl.main__text}>
               Получи все нужные навыки для заработка на NFT всего за 28 дней!
             </p>
-            <button onClick={() => setModalVisible(true)} className="main__btn">
+            <button
+              onClick={() => setModalVisible(true)}
+              className={cl.main__btn}
+            >
               Начать зарабатывать на NFT
             </button>
             <img
-              className="image__light2"
+              className={cl.image__light2}
               src="/images/Lightning 2.png"
               alt="Иконка молнии"
             />
           </article>
-          <div className="main__images">
+          <div className={cl.main__images}>
             <img
-              className="image__elipse"
+              className={cl.image__elipse}
               src="/images/Ellipse 24.png"
               alt="Абстрактное изображение"
             />
             <img
-              className="image__man"
+              className={cl.image__man}
               src="/images/business-3d-businessman-in-blue-suit-meditating 1.png"
               alt="Медитирующий бизнесмен"
             />
             <img
-              className="image__piggy"
+              className={cl.image__piggy}
               src="/images/piggy.png"
               alt="Копилка"
             />
             <img
-              className="image__money"
+              className={cl.image__money}
               src="/images/money.png"
               alt="Деньги"
             />
             <img
-              className="image__smyle"
+              className={cl.image__smyle}
               src="/images/image 41.png"
               alt="Смайлик"
             />
             <img
-              className="image__light1"
+              className={cl.image__light1}
               src="/images/Lightning 1.png"
               alt="Иконка молнии"
             />
